50bc7f0af4edd2341a92a054510bb00c81cda189
Liang-Bo Wang <me@liang2.tw>
2018-04-17 16:31:18 -0500

    Rename to d3scale

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app" class="container">
     <HelloD3 :csvURL='csvURL'></HelloD3>
+    <D3Scales></D3Scales>
     <!-- <img src="./assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"></HelloWorld> -->
   </div>
@@ -9,13 +10,15 @@
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import HelloD3 from './components/HelloD3.vue'
+import D3Scales from './components/D3Scales.vue'
 import csvURL from './assets/data/food.csv'
 
 export default {
   name: 'app',
   components: {
     HelloWorld,
-    HelloD3
+    HelloD3,
+    D3Scales
   },
   data: function () {
     return {
---
+++ src/components/D3Scales.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="d3-root">
+    <h3>D3 scales</h3>
+    <svg></svg>
+  </div>
+</template>
+
+<script>
+import * as d3 from 'd3'
+
+export default {
+  name: 'D3Scales',
+  props: {},
+  mounted: function () {
+    const vm = this.$el
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
