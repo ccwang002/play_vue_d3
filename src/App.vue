ad890c6e0d558a5bfb104aa8132307a1e0444d64
Liang-Bo Wang <me@liang2.tw>
2018-04-18 00:10:10 -0500

    Learning D3.js scales

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app" class="container">
     <HelloD3 :csvURL='csvURL'></HelloD3>
-    <D3Scales></D3Scales>
+    <D3Scales :dataset='scalesDataset'></D3Scales>
     <!-- <img src="./assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"></HelloWorld> -->
   </div>
@@ -13,6 +13,11 @@ import HelloD3 from './components/HelloD3.vue'
 import D3Scales from './components/D3Scales.vue'
 import csvURL from './assets/data/food.csv'
 
+const scalesDataset = [
+  [5, 20], [480, 90], [250, 50], [100, 33], [330, 95],
+  [410, 12], [475, 44], [25, 67], [85, 21], [220, 88]
+]
+
 export default {
   name: 'app',
   components: {
@@ -22,7 +27,8 @@ export default {
   },
   data: function () {
     return {
-      csvURL: csvURL
+      csvURL,
+      scalesDataset
     }
   }
 }
--- src/components/D3Scales.vue
+++ src/components/D3Scales.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="d3-root">
+  <div class="d3-scales">
     <h3>D3 scales</h3>
     <svg></svg>
   </div>
@@ -10,12 +10,52 @@ import * as d3 from 'd3'
 
 export default {
   name: 'D3Scales',
-  props: {},
+  props: {
+    dataset: Array
+  },
   mounted: function () {
     const vm = this.$el
+    const w = 500
+    const h = 400
+    const padding = 20
+
+    const xScale = d3.scaleLinear()
+      .domain([0, d3.max(this.dataset, d => d[0])])
+      .range([padding, w - padding * 2])
+      .nice()
+    const yScale = d3.scaleLinear()
+      .domain([0, d3.max(this.dataset, d => d[1])])
+      .range([h - padding, padding])
+      .nice()
+
+    const svg = d3.select(vm).select('svg')
+      .attr('width', w)
+      .attr('height', h)
+    svg
+      .selectAll('circle')
+      .data(this.dataset)
+      .enter()
+      .append('circle')
+      .attr('cx', d => xScale(d[0]))
+      .attr('cy', d => yScale(d[1]))
+      .attr('r', 10)
+    svg
+      .selectAll('text')
+      .data(this.dataset)
+      .enter()
+      .append('text')
+      .attr('x', d => xScale(d[0]))
+      .attr('y', d => yScale(d[1]))
+      .text(d => `(${d[0]}, ${d[1]})`)
   }
 }
 </script>
 
-<style scoped lang="scss">
+<style lang="scss">
+.d3-scales svg {
+  text {
+    fill: purple;
+    font-size: 0.75em;
+  }
+}
 </style>
